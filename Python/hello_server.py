b8103e5842fbe2eab2a9c748224a04be567906a8
kimhj3715 <root@BK-MacBook-Air.local>
2015-05-21 01:33:22 -0700

    hello_server.py hello_clinet.py

--- Python/hello_server.py
+++ Python/hello_server.py
@@ -2,14 +2,28 @@
 import socket
 import sys
 
+def main(port):
+	message = "hello world"
 
-# create a server socket
-serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	# create a server socket
+	serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# bind the socket to the port
-serv_addr = ('localhost', 8005)
-print >> sys.stderr, 'starting up on %s port %s' % serv_addr
-serv_sock.bind(serv_addr)
+	# bind the socket to the port
+	serv_addr = ('localhost', int(port))
+	print >> sys.stderr, 'starting up on %s port %s' % serv_addr
+	
+	try:
+		serv_sock.bind(serv_addr)
+		serv_sock.listen(5)
+	except socket.error as msg:
+		serv_sock.close()
+
+	clnt_sock, clnt_addr = serv_sock.accept()
+	print 'Connected by', clnt_addr
+
+	clnt_sock.send(message)
+	clnt_sock.close()
+	serv_sock.close()
 
 if __name__ == "__main__":
 	if(len(sys.argv) != 2):
