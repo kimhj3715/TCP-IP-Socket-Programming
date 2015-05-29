b36d1226da350358278c5a70fa92e2a35bb6c5a9
root <root@localhost.localdomain>
2015-05-28 23:17:27 -0700

    thr_server.py thr_client.py

---
+++ Python/th_server/thr_client.py
@@ -0,0 +1,45 @@
+# thr_server.py
+
+import threading, socket, sys
+
+ip = 0
+port = 0
+
+
+def main():
+	# initialize server address
+	sock_addr = (ip, int(port))
+
+	# creat a server socket
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	print >> sys.stderr, 'Connecting to %s port %s' % sock_addr
+	sock.connect(sock_addr)
+
+	while 1:
+		send_data = raw_input("Input a message('Q' or 'q' to quit): ")
+		print >> sys.stderr, 'sending "%s"' % send_data
+		sock.sendall(send_data)
+
+		# Look for the response
+		amount_received = 0
+		amount_expected = len(send_data)
+
+		while amount_received < amount_expected:
+			recv_data = sock.recv(16)
+			amount_received += len(recv_data)
+			print >> sys.stderr, 'received "%s"' % recv_data
+
+
+		if(send_data is 'q' or send_data is 'Q'):
+			print >> sys.stderr, 'closing socket. '
+			sock.close()
+			break
+	return 0
+
+if __name__ == "__main__":
+	if(len(sys.argv) != 3):
+		print >> sys.stderr, "Usage : %s <IP> <port>" % sys.argv[0]
+	else:
+		ip = sys.argv[1]
+		port = sys.argv[2]
+		main()
